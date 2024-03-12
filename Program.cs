f38794887efb01fcaa6eafe57596470c1edc8ddd
Threemasks12 <jongeorgmagnussen@gmail.com>
2024-03-12 10:25:03 +0100

    added alot, pluss baske, and base url

--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
-﻿const string CONFIG = ".config";
+﻿using System.Net.Http.Headers;
+
+
+const string CONFIG = ".config";
 string pantry_id = "";
 
-if(File.Exsist(CONFIG))
+HttpClient httpClient = InitzalizeHttpClient();
+string baseURL = $"http://getpantry.cloud/apiv1/pantry/{pantry_id}/";
+string basketURL = $"{baseURL}basket/";
+
+if(File.Exists(CONFIG))
 {
     string[] configDetails = File.ReadAllLines(CONFIG);
+    pantry_id = configDetails[0];
+}
+else
+{
+    Console.WriteLine("input your Pantry ID");
+    pantry_id = Console.ReadLine() ?? "";
+    if(pantry_id.Length > 0 )
+    {
+        File.WriteAllText(CONFIG, pantry_id);
+    }
+}
+
+HttpClient InitzalizeHttpClient()
+{
+    HttpClient httpClient = new HttpClient();
+    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("applicastion/json"));
+    return httpClient;
 }
